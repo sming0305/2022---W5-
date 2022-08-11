f4e04f29c05ebfeb4b55e7a607373f8f88c1f247
YI <sming0305@gmail.com>
2022-08-11 10:36:40 +0800

    Update 2022-08-11T02:36:36.956Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,24 +1,17 @@
+"use strict";
 
 $(document).ready(function () {
   $(".mainList__view__openButton").click(function (e) {
     e.preventDefault();
     $(".mainList__view").toggleClass("open");
   });
-
-
-
-
-
   $(".mainBlock__menu__edit-dark").click(function (e) {
     e.preventDefault();
     $(".mainBlock").addClass("change");
   });
-
   $(".mainBlock__reply__button__light").click(function (e) {
     e.preventDefault();
     $(".mainBlock").removeClass("change");
   });
-
 });
-
-
+//# sourceMappingURL=all.js.map
