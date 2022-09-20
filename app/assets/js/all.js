19edac4d3ae8949c73443fec5f8ba30ab0eb3a0a
YI <sming0305@gmail.com>
2022-09-20 15:20:56 +0800

    依據助教建議修改完成 & 筆記

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -6,7 +6,13 @@ $(document).ready(function () {
   });
 
 
+  // 左側選單可參考文件，在當前頁面的 li 加上 .active 這個 class，
+  // 讓使用者清楚目前處於哪個頁面
 
+  $(".Nav__list__item").click(function (e) {
+    $(this).toggleClass("active");
+    $(this).parent().siblings().children("a").removeClass("active");
+  });
 
 
   $(".mainBlock__menu__edit-dark").click(function (e) {
@@ -19,6 +25,41 @@ $(document).ready(function () {
     $(".mainBlock").removeClass("change");
   });
 
+
+// 推到 GitHub 會無效主要是因為本地與 GitHub 路徑有差異造成的唷
+// 由於 location.pathname 是取得網頁路徑（path），在推到 GitHub 後路徑會改變，需要改為以下寫法才能正確讀取
+// window.location.pathname == '/你的專案名稱(/2022---W5-)/index.html'
+// 在本地正常推到 GitHub 後無效的狀況大多都是路徑問題引起，日後遇到類似問題也可先朝這個方向嘗試排錯
+// 可以再調整看看，若還有任何問題歡迎隨時提出
+
+// https://sming0305.github.io/2022---W5-/index.html
+// https://sming0305.github.io/2022---W5-/admin.html
+// protocol://host.domain/path/filename
+
+  $(function () {
+    if(window.location.pathname == '/2022---W5-/index.html') {
+      $("#assignment").addClass("active");
+    } else if(window.location.pathname == '/2022---W5-/admin.html') {
+      $("#admin").addClass("active");
+    }
+  });
+
+
+//本地端使用  
+// http://localhost:8080/index.html
+// protocol://host.domain/path/filename
+
+//   $(function () {
+//     if(window.location.pathname == '/index.html') {
+//       $("#assignment").addClass("active");
+//     } else if(window.location.pathname == '/admin.html') {
+//       $("#admin").addClass("active");
+//     }
+//   });
+
+
+
 });
+
 
 
